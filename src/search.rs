dddb460881ed132462f57ec401ae8f5c8b93002a
Hamish Miller <hamishcomiller@googlemail.com>
2020-10-19 20:11:52 +0100

    Defend existing search behaviour

--- src/search.rs
+++ src/search.rs
@@ -18,6 +18,7 @@ pub enum SearchMode {
     Full,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct SearchSummary {
     pub is_match: bool,
     pub count: Option<usize>,
@@ -44,3 +45,71 @@ impl fmt::Display for SearchSummary {
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_singleline_macro() {
+        let cpp = "MOCK_METHOD1(Foo, bool(int))";
+
+        assert!(search(&cpp, SearchMode::Lazy).is_match);
+    }
+
+    mod lazy {
+        use super::*;
+
+        #[test]
+        fn test_zero_match() {
+            let cpp = "";
+            let expected = SearchSummary { is_match: false, count: None };
+
+            assert_eq!(search(&cpp, SearchMode::Lazy), expected);
+        }
+
+        #[test]
+        fn test_single_match() {
+            let cpp = "MOCK_METHOD1(Foo, bool(int))";
+            let expected = SearchSummary { is_match: true, count: None };
+
+            assert_eq!(search(&cpp, SearchMode::Lazy), expected);
+        }
+
+        #[test]
+        fn test_multi_match() {
+            let cpp = "MOCK_METHOD1(Foo, bool(int))\nMOCK_METHOD1(Bar, bool(int))";
+            let expected = SearchSummary { is_match: true, count: None };
+
+            assert_eq!(search(&cpp, SearchMode::Lazy), expected);
+        }
+    }
+
+    mod full {
+        use super::*;
+
+        #[test]
+        fn test_zero_match() {
+            let cpp = "";
+            let expected = SearchSummary { is_match: false, count: Some(0) };
+
+            assert_eq!(search(&cpp, SearchMode::Full), expected);
+        }
+
+        #[test]
+        fn test_single_match() {
+            let cpp = "MOCK_METHOD1(Foo, bool(int))";
+            let expected = SearchSummary { is_match: true, count: Some(1) };
+
+            assert_eq!(search(&cpp, SearchMode::Full), expected);
+        }
+
+        #[test]
+        fn test_multi_match() {
+            let cpp = "MOCK_METHOD1(Foo, bool(int))\nMOCK_METHOD1(Bar, bool(int))";
+            let expected = SearchSummary { is_match: true, count: Some(2) };
+
+            assert_eq!(search(&cpp, SearchMode::Full), expected);
+        }
+    }
+}
